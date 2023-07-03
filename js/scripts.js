2af0b25f7d745b144d50bf99c7a280577d019bdc
Esteban Sánchez <esteban.sanchez@gmail.com>
2023-07-03 12:28:08 +0200

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -214,11 +214,10 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Un momento</strong>. Estamos guardando los datos...'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+        if (MD5($('#invite_code').val()) !== 'f9d89a2d5268f2699a7d63ac669e001f') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbyo0rEknln8LedEP3bkONsfOh776IR5lFidLhJFQ6jdvRiH4dKvHZmtoIybvnxpxYr2cA/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbykMYasN4QNl3fXx7tDCMjzcFGXt_eO1VCL0egTWuGbyNn7-wRTKRvivuKjln1RLKE2/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
